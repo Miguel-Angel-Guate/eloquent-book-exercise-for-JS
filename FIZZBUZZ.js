742ca71b1916d5f7f4f4dade23247830d01357b7
Miguel-Angel-Guate <miguelangelteletor@yahoo.es>
2024-04-23 20:32:59 +0200

    fizz buzz code

---
+++ FIZZBUZZ.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i++) {
+    let printing = (i % 3 == 0 ? "FIZZ" : "") + (i % 5 == 0 ? "BUZZ" : "");
+    console.log(`${i} -- ${printing || i}`);
+    /* let printing = "";
+    if (i % 3 == 0) printing += "Fizz";
+    if (i % 5 == 0) printing += "Buzz";
+    if(i % 3 != 0 && i % 5 != 0) printing = "FIzzBuzz";
+    console.log(`${i}- ${printing}`); */
+
+}
--- triangle.js
+++ triangle.js
@@ -1,3 +1,12 @@
 for (let i = "#"; i.length <= 7; i+= "#") {
     console.log(i)
 }
+
+let output = "";
+    if (i % 3 == 0) output += "Fizz";
+    if (i % 5 == 0) output += "Buzz";
+    if(i % 3 != 0 && i % 5 != 0) output = "FIzzBuzz";
+    console.log(`${i}- ${output}`);
+
+    /* if(i % 3 == 0) printing += "FIZZ"
+    if(i % 5 === 0) printing += "BUZZ" */
